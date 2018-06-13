12e26bb7f7c2136a55ef483a2b7e9ca828db5fb1
Leo-xh <1134086740@qq.com>
2018-06-13 16:25:45 +0800

    basic

---
+++ src/predict.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
+#include <vector>
+#include <omp.h>
+
+using namespace std;
+
+vector<int> predict()
+{
+
+}
+
+
+int main(int argc, char const *argv[])
+{
+	return 0;
+}
